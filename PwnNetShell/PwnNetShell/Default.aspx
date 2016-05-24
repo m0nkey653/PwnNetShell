c547803cb62bee5c70ca9960cc6247de515dc021
Shane Dabirsiaghi <shane.dabirsiaghi@contrastsecurity.com>
2016-05-24 10:28:32 -0400

    Imported to github

---
+++ PwnNetShell/PwnNetShell/Default.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="PwnNetShell._Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <h1>Vulnerable Upload Page</h1>
+
+        <p>
+            This page updloads a file to the web application without any checks and puts it in the root of
+            the application folder. This location is directly browsable, so an uploaded ASPX page will execute.
+        </p>
+
+        <h3>Select file to upload:</h3>
+        <asp:FileUpload runat="server" ID="fuVuln" />
+        <br />
+        <asp:Button runat="server" ID="btnUpload" OnClick="btnUpload_OnClick" Text="Upload File" />
+        <br />
+        <asp:Label runat="server" ID="lblMessage" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ PwnNetShell/PwnNetShell/Default.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PwnNetShell
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process p = new System.Diagnostics.Process();
+            p.OutputDataReceived += P_OutputDataReceived;
+            p.ErrorDataReceived += P_ErrorDataReceived;
+        }
+
+        private void P_ErrorDataReceived(object sender, System.Diagnostics.DataReceivedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void P_OutputDataReceived(object sender, System.Diagnostics.DataReceivedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected void btnUpload_OnClick(object sender, EventArgs e)
+        {
+            if (fuVuln.HasFile)
+            {
+                fuVuln.SaveAs(Request.MapPath("~/") + System.IO.Path.GetFileName(fuVuln.PostedFile.FileName));
+                lblMessage.Text = "File has been saved to: " + Request.MapPath("~/") + System.IO.Path.GetFileName(fuVuln.PostedFile.FileName);
+            }
+
+
+        }
+    }
+}
